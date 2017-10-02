2b105fa120c42fa862a166f47d4c5e0c43818020
Desmond O. Chang <dochang@gmail.com>
2017-10-03 06:26:18 +0800

    Bump version to 0.0.9

--- bumplus/version.py
+++ bumplus/version.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ long_description = re.sub(
 
 setup(
     name='bumplus',
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/dochang/bumplus',
     author='Desmond O. Chang',
     author_email='dochang@gmail.com',
