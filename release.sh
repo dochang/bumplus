d1e55a478e0b02f524d6fefb73c5d1489f9feef4
Desmond O. Chang <dochang@gmail.com>
2018-02-25 20:47:25 +0800

    Use pipenv to build & upload releases

--- release.sh
+++ release.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-cd $(dirname $0)
-
-venv_dir=venv/release
-dist_dir=dist
-
-rm -rf $dist_dir $venv_dir
-
-virtualenv $venv_dir
-. $venv_dir/bin/activate
-pip install -U twine
-
-python setup.py sdist
-twine upload $dist_dir/*
+pipenv run python setup.py sdist bdist_wheel
+pipenv run twine upload dist/*
+rm -rf build dist *.egg *.egg-info
