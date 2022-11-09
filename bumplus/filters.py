f42fa573e9789e99502a9f73c3034874dbdb45d8
Wade Zhang <dochang@gmail.com>
2022-11-09 21:42:40 +0800

    Fix [W0622]
    [W0622]: https://pylint.pycqa.org/en/latest/user_guide/messages/warning/redefined-builtin.html#redefined-builtin-w0622

--- bumplus/filters.py
+++ bumplus/filters.py
@@ -2,16 +2,16 @@
 from __future__ import absolute_import, division, print_function
 
 
-def strftime(value, format="%x"):
+def strftime(value, format_spec="%x"):
     """Template filter to format data and time.
 
     :param value:
         A datatime object.
-    :param format:
+    :param format_spec:
         A format string which is the same as the parameter of ``strftime``
         method of Python ``datatime`` object.
     """
-    return value.strftime(format)
+    return value.strftime(format_spec)
 
 
 filters = {"strftime": strftime}
